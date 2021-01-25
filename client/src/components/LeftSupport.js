49fa2f9296a4ffb3eaa308109161f86c2eccb09e
Elias <elias@baez.us>
2021-01-25 15:43:01 -0500

    Figured out why the font wasn't working quite right. I'm not sure if it's the best font.

--- client/src/components/LeftSupport.js
+++ client/src/components/LeftSupport.js
@@ -4,7 +4,7 @@ const LeftSupport = () => {
 
   return (
     <div>
-      <p className="is-size-5 has-font-weight-semibold">This is a poem called <strong>"Skin in the Game."</strong> It has been printed in Mantis magazine, and in the Amherst College alumni magazine.</p>
+      <p className="is-size-5 has-text-weight-semibold">This is a poem called <strong>"Skin in the Game."</strong> It has been printed in Mantis magazine, and in the Amherst College alumni magazine.</p>
     </div>
   )
 
--- client/src/components/RightActive.js
+++ client/src/components/RightActive.js
@@ -4,7 +4,7 @@ const RightActive = () => {
 
   return (
     <div className="box">
-      <p className="is-size-4">I found a book on Triangle Street -- <br/>
+      <p className="is-size-4 has-text-weight-bold">I found a book on Triangle Street -- <br/>
       I, eighteen, was passing by. <br/>
       Page three spelled out a unicorn <br/>
       Spearing a poet's eye.<br/><br/>
