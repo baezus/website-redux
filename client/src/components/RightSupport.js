76ae4ea3bd3efd392da200823ec228a9efc7fc0b
Elias <elias@baez.us>
2021-01-25 18:57:08 -0500

    Added a little animation

--- client/src/components/RightSupport.js
+++ client/src/components/RightSupport.js
@@ -3,9 +3,10 @@ import React from 'react';
 const RightSupport = () => {
 
   return (
-    <div className="is-justify-content-right">
+    <div className="is-justify-content-right"><br/>
       <p className="is-size-6 has-text-weight-semibold">I'm currently reading <strong>Black Hat Go: Go Programming for Hackers and Pentesters</strong> by Tom Steele, Chris Patten, and Dan Kottmann.</p><br/>
-      <p className="is-size-6 has-text-weight-semibold">I want to supplement my React and JavaScript foundation with languages like Go because network connectivity, servers in the clouds or otherwise, are what most interest me.</p>
+      <p className="is-size-6 has-text-weight-semibold">I want to supplement my React and JavaScript foundation with languages like Go because network connectivity, servers in the clouds or otherwise, are what most interest me.</p><br/>
+      <p className="is-size-6 has-text-weight-semibold">Since encountering Bulma (the CSS library, not the Dragonball character) and getting comfortable with SASS, I've become more interested in design as well.</p>
     </div>
   )
 
