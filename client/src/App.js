41144c3edf904e5e98aef8d97674649232c00ed7
Elias <elias@baez.us>
2021-01-25 05:32:01 -0500

    Added React hooks.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React, { useEffect, useState } from 'react';
 import './styles/App.scss';
 
+
 function App() {
+
+  const [isHome, setIsHome] = useState(true);
+  const [path, setPath] = useState('home');
+
+  useEffect(() => {
+    console.log('use effect hook ran');
+  }, [])
+
   return (
     <div className="App">
       <p>Elias Baez</p>
+      <p>you are {path}</p>
     </div>
   );
 }
